cdf38c05a2e50f8716c2a3ee6ac95c6e0de4eec3
suzytoussaint <suzy_toussaint@orange.fr>
2020-04-03 15:04:58 +0200

    Exo 2

---
+++ tp1/Exercice 2.py
@@ -0,0 +1,11 @@
+i=0
+print("Boucle Pour")
+for i in range(2, 20, 2):  # pour i allant de 1 à 5 par pas de 2
+    print(f"--> i vaut: {i}")  # je repète l'action 2 fois
+
+#En utilisant une boucle tant que
+print("Boucle TantQue:")
+x = 0
+while x < 21:
+    print(f"--> x vaut: {x}")  # je repète l'action 5 fois
+    x += 2
