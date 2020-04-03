2f0c02f4379e849c1a61a970aeff295dfb6d7764
suzytoussaint <suzy_toussaint@orange.fr>
2020-04-03 16:39:35 +0200

    Exo 3

--- tp1/Exercice 4.py
+++ tp1/Exercice 4.py
@@ -1,8 +1,10 @@
 n = 0
 i = 0
-n = int (input("Saisire entre 1 et 100"))
+n = int (input("Saisire entre 1 et 100 : "))
 if n < 1 or n > 100 :
 
     print (n, "n'est pas entre 1 et 100")
 else :
-    print(f"--> i vaut: {i}")
+    print("Boucle Pour ")
+    for i in range(1, 1 * n ):
+        print(i)
