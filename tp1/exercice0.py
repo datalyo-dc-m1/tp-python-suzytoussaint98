796bf624e3491f74b534c3ae54ccc7736a6720bb
suzytoussaint <suzy_toussaint@orange.fr>
2020-04-03 14:24:00 +0200

    Exo 0

---
+++ tp1/exercice0.py
@@ -0,0 +1,15 @@
+a = 5.3
+b = 9.70
+c = a + b
+print(c) #c vaut 15
+
+c += a
+d = c
+d -= 1
+print(d) #d vaut 19,3
+
+print ( type ( c )) # c est un float
+
+c_int  =  float ( input ( "15" )) #la fonction input affiche directement le résultat
+
+
